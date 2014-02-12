18c1b37a88febb79fda81eccda457fb3d5f039d7
NothingSethual <sethcallaway@gmail.com>
2014-02-11 22:55:19 -0500

    Port to RoM

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var query_params = { apikey: 'a12b17ca0d5f2bfba41ee42ec0f051a1',
                      phrase: 'atheist',
+                     
+                     start_date: '2014-01-09',
+                     end_date: '2014-02-14'
                    };
 var endpoint = 'http://capitolwords.org/api/text.json';
 
